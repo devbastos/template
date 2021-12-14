5cbdcb9c3307507cc13393839659696325b52bf7
Gabriel Bastos <gbastosbarbosa@gmail.com>
2021-12-14 19:22:27 -0300

    first comit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # template
- entendo um pouco mais sobre strings
+ entendo um pouco mais sobre strings e usando ##template ``
+
+<img src="./assets/img/veja.jpeg" alt="imagem do resultado no console">
---
+++ scripts.js
@@ -0,0 +1,8 @@
+const empresa = 'eco74';
+const emailEmpresa = 'contato@eco74.com.br';
+const contato = '(11) 97186-8122';
+const desconto = 'a123';
+
+const nome = 'Gabriel';
+
+const emailMarketing = ` Ola ${nome}, Nós da ${empresa} Agradecemos a preferencia. Aqui vai seu código de desconto na sua primeira compra, ${desconto}`;
